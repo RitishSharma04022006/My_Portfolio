c58d5405727838214189be037c6fa8bc90ca9ec9
Ritish Sharma <ritishsharma04022006@gmail.com>
2025-01-03 15:59:09 +0530

    Adding Footer And Some Minor Changes In The Codes

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 let hamesburger=document.querySelector('#hamesburger');
 let navcontent=document.querySelector('.nav-content');
 let cross=document.querySelector('#cross');
+let navcontentlinks=document.querySelectorAll('.nav-content .links');
+console.log(navcontentlinks);
+
 hamesburger.addEventListener('click',()=>{
         navcontent.style.left=`${0}%`;
         hamesburger.style.display='none';
         cross.style.display='inline-block';
 });
+
 cross.addEventListener('click',()=>{
     navcontent.style.left=`${-100}%`;
     cross.style.display='none';
     hamesburger.style.display='inline-block';
 });
+
+navcontentlinks.forEach((el)=>{
+    el.addEventListener('click',()=>{
+        navcontent.style.left=`${-100}%`;
+        cross.style.display='none';
+        hamesburger.style.display='inline-block';
+    });
+})
 
